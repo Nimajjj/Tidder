af4db630cf9daf919f49e6815a56eb848bc278fe
ben <benjamin.borello@ynov.com>
2022-03-22 20:22:06 +0100

    start subtidder cration back end

--- Application/go/sql/methods.go
+++ Application/go/sql/methods.go
@@ -38,6 +38,20 @@ func (sqlServ SqlServer) GetAccountById(id int) Accounts {
   return account
 }
 
+// /!\ Not finished yet /!\ //
+// INSERT INTO `subjects` (name, profile_picture, id_owner, nsfw) VALUES ("Dank Meme", "default.png", 1, 0)
+func (sqlServ SqlServer) CreateSub(subName string, ownerId int, nsfwInput bool) {
+  if len(subName) >= 25 || ownerId < 1 { return }
+
+  nsfw := 0
+  if nsfwInput { nsfw = 1 }
+
+  query := "INSERT INTO `subjects` (name, profile_picture, id_owner, nsfw) VALUES ("
+  query += "\"" + subName + "\", \"default.png\", "
+  query += strconv.Itoa(ownerId) + ", " + strconv.Itoa(nsfw)
+  sqlServ.executeQuery(query)
+}
+
 
 func (sqlServ SqlServer) executeQuery(query string) {
   _, err := sqlServ.db.Query(query)
