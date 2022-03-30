3709c922cda0bd905d812e3686440b97ee01bbd4
ben <benjamin.borello@ynov.com>
2022-03-30 17:25:05 +0200

    link subscribe bt to sql db

--- Application/go/server/handlers.go
+++ Application/go/server/handlers.go
@@ -1,12 +1,13 @@
 package server
 
 import (
+  "encoding/json"
   "strconv"
   "strings"
   "html/template"
   "net/http"
 
-
+  Util "github.com/Nimajjj/Tidder/go/utility"
   SQL "github.com/Nimajjj/Tidder/go/sql"
 )
 
@@ -39,7 +40,9 @@ func SubtidderHandler(db *SQL.SqlServer) {
 
   tpl := template.Must(template.ParseFiles("./pages/subtidder.html"))
   http.HandleFunc("/t/", func(w http.ResponseWriter, r *http.Request) {
-    if r.FormValue("name") != "" {        // create subtidder
+    // CREATE SUBTIDDER COMPONENT //////////////////////////////////////////////////
+
+    if r.FormValue("name") != "" {       
       subTidderName := r.FormValue("name")
       subTidderNsfw := false
       if r.FormValue("nsfw") == "0" {
@@ -48,6 +51,21 @@ func SubtidderHandler(db *SQL.SqlServer) {
       db.CreateSub(subTidderName, 2, subTidderNsfw)
     }
 
+    // END CREATE SUBTIDDER COMPONENT //////////////////////////////////////////////
+    // SUBSCRIBE TO SUBTIDDER COMPONENT ////////////////////////////////////////////
+
+    type Subscription struct {
+      IdAccount int `json:"id_account_subscribing"`
+      IdSubject int `json:"id_subject_to_subscribe"`
+    }
+    subscription := &Subscription{}
+    json.NewDecoder(r.Body).Decode(subscription)
+    if subscription.IdAccount != 0 && subscription.IdSubject != 0 {
+      db.SubscribeToSubject(subscription.IdAccount, subscription.IdSubject)
+    }
+
+    // END SUBSCRIBE TO SUBTIDDER COMPONENT ////////////////////////////////////////
+    // MAIN SUBTIDDER COMPONENT ////////////////////////////////////////////////////
     
     id := strings.ReplaceAll(r.URL.Path, "localhost/t/", "")
 		id = strings.ReplaceAll(r.URL.Path, "/t/", "")
@@ -76,6 +94,7 @@ func SubtidderHandler(db *SQL.SqlServer) {
     }
 
     tpl.Execute(w, subtidder)
+    // END MAIN SUBTIDDER COMPONENT ////////////////////////////////////////////////
   })
 }
 
--- Application/scripts/master.js
+++ Application/scripts/master.js
@@ -14,3 +14,19 @@ if (document.getElementById("create_subtidder_error").innerHTML != "") {
 } else {
   hide_popup()
 }
+
+
+function SubscribeTo(id_account, id_subject) {
+  fetch(location.pathname, {
+    method: "post",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  
+    //make sure to serialize your JSON body
+    body: JSON.stringify({
+      "id_account_subscribing": id_account,
+      "id_subject_to_subscribe": id_subject
+    })
+  })
+}
